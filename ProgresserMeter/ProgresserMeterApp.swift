6a543ab0bd2a71338b38972c864b2743652c5108
Steven Zhang <YZhang@lcs.on.ca>
2021-02-16 14:31:42 -0500

    Initial Commit

---
+++ ProgresserMeter/ProgresserMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgresserMeterApp.swift
+//  ProgresserMeter
+//
+//  Created by Steven Zhang on 2021-02-16.
+//
+
+import SwiftUI
+
+@main
+struct ProgresserMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
